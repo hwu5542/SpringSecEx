1965cdc244c0f28def90e6bb95345d9272ff9b00
wuho <hwu5542@gmail.com>
2025-03-05 08:47:53 -0600

    initial commit

---
+++ src/test/java/com/wuho/SpringSecEx/SpringSecExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuho.SpringSecEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

319a55bcc00237e972c9c2c405d4c17833d4129c
wuho <hwu5542@gmail.com>
2025-03-13 00:53:41 -0500

    Kafka v2

--- src/main/java/com/wuho/SpringSecEx/service/KafkaService.java
+++ src/main/java/com/wuho/SpringSecEx/service/KafkaService.java
@@ -3,6 +3,7 @@ package com.wuho.SpringSecEx.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -17,5 +18,10 @@ public class KafkaService {
 
     public void sendMsg(String msg) {
         kafkaProducerService.sendSchoolMessage(msg);
+    }
+
+    @KafkaListener(topics = "school", groupId = "groupId")
+    public void listener(String data) {
+        System.out.println("Application received: " + data );
     }
 }
